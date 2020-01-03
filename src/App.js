bddcec5ae07a8c3d9255454ebbf5c7e2d2fc6173
Natalia Barriga <nbarrigam@MacBook-Air-de-Natalia.local>
2020-01-03 13:43:08 -0300

    botones de mesero, desayuno y almuerzo con rutas asociadas, algunas correcciones en css y se muestra la data

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 import { useFirebaseApp } from "reactfire";
-//import LunchMenu from "./views/LunchMenu";
-//import BreakfastMenu from "./views/BreakfastMenu";
-import Home from "./views/Home";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-//import React, {Component} from "react";
-//import Container from "./Componentes/CentralWhiteMenu.js";
+import Home from "./Def Vistas/Home.js";
+
 
 function App() {
   const firebase = useFirebaseApp();
   console.log(firebase);
 
   return (
-    <Router>
-      <div className="root">
-        <header className="App-header">
-          <Home />
-        </header>
-      </div>
-    </Router>
+      <Home/>
   );
 }
-//class App extends Component{
-
-//  render(){
-//    return <div>
-//      <Container/>
-//      </div>
-//  }
-//}
 
 
 export default App;
