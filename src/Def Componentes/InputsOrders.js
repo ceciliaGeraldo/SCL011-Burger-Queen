4ef67f32638b0df7fe46cde2931c35d4554dc8c6
Natalia Barriga <nbarrigam@MacBook-Air-de-Natalia.local>
2020-01-08 13:34:01 -0300

    commit para bajar

--- src/Def Componentes/InputsOrders.js
+++ src/Def Componentes/InputsOrders.js
@@ -1,67 +1,67 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import db from "../firebaseConfig.js"
 
 
-class Inputs extends Component{
-
-        state = {
-            orders:[],
-            inputName:"",
-            inputNumber:"",
+class Inputs extends Component {
+    constructor() {
+        super()
+        this.state = {
+            orders: [],
+            inputName: "",
+            inputNumber: "",
         }
+    }
 
-    componentDidMount(){
-        db.collection("customersAndOrders").onSnapshot((snapShots)=>{
+    componentDidMount() {
+        db.collection("customersAndOrders").onSnapshot((snapShots) => {
             this.setState({
-                orders: snapShots.docs.map(doc =>{
-                    return {id:doc.id, data:doc.data()}
+                orders: snapShots.docs.map(doc => {
+                    return { id: doc.id, data: doc.data() }
                 })
             })
         })
     }
 
-    changeValueName = (e) =>{
+    changeValueName = (e) => {
         this.setState({
-            inputName:e.target.value,
+            inputName: e.target.value,
         })
     }
 
-    changeValueNumber = (e) =>{
+    changeValueNumber = (e) => {
         this.setState({
-            inputNumber:e.target.value,
+            inputNumber: e.target.value,
         })
     }
 
-    action = () =>{
-        const{inputName, inputNumber} = this.state;
+    action = () => {
+        const { inputName, inputNumber } = this.state;
         db.collection("customersAndOrders").add({
-            orders:{inputName, inputNumber}
-        }).then(()=>{
+            orders: { inputName, inputNumber }
+        }).then(() => {
             console.log("agregado");
-        }).catch(()=>{
+        }).catch(() => {
             console.log("error");
         })
     }
- 
 
+    render() {
 
-    render(){
+        const { inputName, inputNumber } = this.state;
 
-        const{orders, inputName, inputNumber} =this.state;
-
-        return(
+        return (
             <div>
-                <input 
-                type="text" 
-                placeholder="Nombre Cliente"
-                value={inputName}
-                onChange={this.changeValueName}
+                <input
+                    type="text"
+                    placeholder="Nombre Cliente"
+                    value={inputName}
+                    onChange={this.changeValueName}
                 />
-                <input 
-                type="number" 
-                placeholder="Nº de mesa"
-                value={inputNumber}
-                onChange={this.changeValueNumber}
+                <input
+                    type="number"
+                    placeholder="Nº de mesa"
+                    value={inputNumber}
+                    onChange={this.changeValueNumber}
                 />
                 <button onClick={this.action}>Enviar a Cocina</button>
             </div>
--- src/Def Componentes/SidebarBlue.js
+++ src/Def Componentes/SidebarBlue.js
@@ -1,19 +1,21 @@
 /* Componente barra lateral derecha azul */
 /* Componentes: - Input nombre cliente, input numero de mesa, pedido realizado, botón enviar cocina */
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './SidebarBlue.css';
 import Inputs from "./InputsOrders.js";
+import db from "../firebaseConfig.js"
 
 class BlueSidebar extends Component {
 
-    render(){
-        return(
-        <div className="sidebarBlue">
-            <Inputs/>
-            <h1 className="orderNumber">Pedido Nº</h1>
-            <button className="btnKitchen">Enviar a Cocina</button>
-        </div>
+
+    render() {
+        return (
+            <div className="sidebarBlue">
+                <Inputs />
+                <h1 className="orderNumber">Pedido Nº</h1>
+                <button className="btnKitchen" onClick={this.action}>Enviar a Cocina</button>
+            </div>
         )
     }
 }
